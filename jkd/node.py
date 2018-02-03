266bc7982cb65fc5925c442ca346f81e0fcda0b5
ajaborsk <alexandre.jaborska@laposte.net>
2018-02-03 08:40:50 +0100

    Preliminary Port (outside) API

--- jkd/node.py
+++ jkd/node.py
@@ -1,6 +1,34 @@
+import asyncio
 from .logging import *
 
 import xml.etree.ElementTree as ET
+
+class Port:
+    def __init__(self):
+        self.input = asyncio.Queue()
+        self.output = asyncio.Queue()
+
+    # Low level API (but maybe using directly Queue API would be sufficent ?)
+    async def send(self, message):
+        await self.input.put(message)
+
+    async def recv(self):
+        return await self.output.get()
+
+    # High level API
+    async def get(self, address = None):
+        #TODO
+        pass
+
+    async def set(self, value, address = None):
+        #TODO
+        pass
+
+    # conditional / complex get
+    async def request(self, query, address = None):
+        #TODO
+        pass
+
 
 class Node:
     def __init__(self, env = None):
