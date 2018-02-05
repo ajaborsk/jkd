83a83f7a52dcd14a94d5b1754234fccf9994a05b
Alexandre JABORSKA <alexandre.jaborska@laposte.net>
2018-02-05 17:17:59 +0100

    First cache implementation

---
+++ jkd/cache.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 14:38:12 2018
+
+@author: jaborsal06
+"""
+
+import datetime
+
+from .node import *
+
+class Cache(Node):
+    tagname = "cache"
+    def __init__(self, contents = None, **kwargs):
+        super().__init__(**kwargs)
+        self.timestamp = None
+        self.contents = None
+        self.sources = [] # inputs list
+        self.subscriptions = [] # output
+    
+    async def aset(self, value):
+        self.contents = value
+        self.timestamp = datetime.datetime()
+        # triggers 
+        #TODO
+        
+    async def aget(self):
+        #TODO
+        pass
