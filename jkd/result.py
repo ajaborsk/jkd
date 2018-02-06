c14fe9f49d15cd6d8951c97a3f000aa5a5ae7c6f
ajaborsk <alexandre.jaborska@laposte.net>
2018-02-06 07:44:13 +0100

    Preliminary result node class

---
+++ jkd/result.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 14:38:12 2018
+
+@author: jaborsal06
+"""
+
+import datetime
+
+import asyncio
+from .node import *
+
+class Result(Node):
+    tagname = "cache"
+    def __init__(self, contents = None, **kwargs):
+        super().__init__(**kwargs)
+        self.timestamp = None
+        self.contents = None
+        self.sources = [] # inputs list
+        self.subscriptions = [] # output
+        self.output = asyncio.Queue()
+        self.input = asyncio.Queue()
+        self.current_result = None
+
+    async def aset(self, value):
+        self.contents = value
+        self.timestamp = datetime.datetime()
+        # triggers
+        #TODO
+
+    async def aget(self):
+        #TODO
+        pass
