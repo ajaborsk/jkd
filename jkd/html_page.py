83536dd22fce763ac656b2d14ec7ef08c502ae04
Alexandre JABORSKA <alexandre.jaborska@laposte.net>
2018-02-12 11:44:27 +0100

    Jinja2 templates capability for html pages

--- jkd/html_page.py
+++ jkd/html_page.py
@@ -1,12 +1,20 @@
+#from jinja2 import Environment, PackageLoader, select_autoescape
+
+import jinja2
+
 from .node import *
 
 class HtmlPage(Node):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.jinja_env = jinja2.Environment(
+                loader=jinja2.FileSystemLoader('templates/'),
+                autoescape=jinja2.select_autoescape(['html', 'xml']))
         
     async def msg_handle(self, msg):
         if 'query' in msg and msg['query'] == 'get':
-            html_page = '<html><body>HtmlPage default reply.</body></html>'
+            template = self.jinja_env.get_template('tmpl.jinja2')
+            html_page = template.render({'name':'Joris'})
             self.debug("handling"+str(msg))
             self.debug("Reply to " + str(msg['src']) + "/ qid=" + str(msg['qid']))
             rep= {'qid':msg['qid'], 'reply':html_page}
