94e23193dac45b1dbaa8ea5fde25dee49395207a
ajaborsk <alexandre.jaborska@laposte.net>
2018-03-07 21:15:00 +0100

    Cleanup

--- jkd/text_parser.py
+++ jkd/text_parser.py
@@ -12,19 +12,9 @@ class TextParser(Node):
         self.port_add('input', mode = 'input')
         self.port_add('output', cached = True, timestamped = True)
         self.task_add('process', coro = self.parse, gets=['input'], returns=['output'])
-        #self.task_add('process_loop', coro = self.output_task, needs=['input'], provides=['output'])
 
     async def parse(self, line):
-        #TODO
         #self.debug('line: '+str(line[1]))
         value = float(line[1][7:])
         #self.debug('value: '+str(value))
         return "#" * int(100. * (value + 100) / 200.)
-
-    async def output_task(self):
-        #TODO: output_task scheduling should be determined by output channels configurations
-        while True:
-            #self.debug(str(self.name) + " : output_task...")
-            line = await self.port_read('input')
-            value = await self.parse(line)
-            await self.port_value_update('output', value)
