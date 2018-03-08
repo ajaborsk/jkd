3d592de04bbf131f28a5b273d385ce375b1931ee
ajaborsk <alexandre.jaborska@laposte.net>
2018-03-08 07:20:30 +0100

    effective code (instead of fake one)

--- jkd/serial_capture.py
+++ jkd/serial_capture.py
@@ -4,6 +4,7 @@ import asyncio
 
 from .node import *
 
+#TODO: Find a way to provide a value on a port without any task (use a integrated one)
 
 class SerialCapture(Node):
 
@@ -15,13 +16,20 @@ class SerialCapture(Node):
         self.serial_baudrate = int(serial_baudrate)
         self.serial = serial.Serial(self.serial_port, self.serial_baudrate)
         self.serial.timeout = 0
+        self.serial_queue = asyncio.Queue()
         self.serial.nonblocking()
         self.current_buffer = b''
-
         self.env.loop.add_reader(self.serial.fd, self._read_ready)
+
+        self.task_add('serial_line_handler', coro = self.serial_line_handler, provides = ['output'], autolaunch=True)
+
+    async def serial_line_handler(self):
+        while True:
+            await self.port_value_update('output', await self.serial_queue.get())
 
     async def serial_message_handle(self, serial_buffer):
         self.debug("Serial message: "+str(serial_buffer))
+        await self.serial_queue.put(serial_buffer)
 
     def _read_ready(self):
         block = self.serial.read(1024)
