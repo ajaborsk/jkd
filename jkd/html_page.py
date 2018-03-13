0152dfa8b8479d2a7260073ef66be955086a51d7
ajaborsk <alexandre.jaborska@laposte.net>
2018-03-13 16:10:28 +0000

    First htmlÃpart implementation

--- jkd/html_page.py
+++ jkd/html_page.py
@@ -6,10 +6,13 @@ from .node import *
 
 class HtmlPart:
     def __init__(self, elt = None, p_class = None, p_name = None, p_id = None, **kwargs):
+        self.p_class = p_class
+        self.p_name = p_name
+        self.p_id = p_id
         self.scripts = []
         self.css = []
-        self.html = ""
-        self.js = ""
+        self.html_template = jinja2.Template("")
+        self.js_template = jinja2.Template("")
         self.kwargs = kwargs
 
     def script_add(self, script_name):
@@ -20,25 +23,120 @@ class HtmlPart:
         if css_name not in self.css:
             self.css.append(css_name)
 
+    def context(self):
+        return {'p_id':self.p_id}
+
+    def get_html(self):
+        return self.html_template.render(self.context())
+
+    def get_js(self):
+        return self.js_template.render(self.context())
+
+
+class HtmlPartValue(HtmlPart):
+    def __init__(self, elt = None, p_class = None, p_name = None, p_id = None, **kwargs):
+        super().__init__(elt, p_class, p_name, p_id, **kwargs)
+
 
 class HtmlPartTable(HtmlPart):
-    def __init__(self, elt = None, p_class = None, p_name = None, p_id = None):
-        pass
+    def __init__(self, elt = None, p_class = None, p_name = None, p_id = None, **kwargs):
+        super().__init__(elt, p_class, p_name, p_id, **kwargs)
 
 
 class HtmlPartChart(HtmlPart):
-    def __init__(self, elt = None, p_class = None, p_name = None, p_id = None):
-        pass
+    def __init__(self, elt = None, p_class = None, p_name = None, p_id = None, **kwargs):
+        super().__init__(elt, p_class, p_name, p_id, **kwargs)
 
 
 class HtmlPartHisto(HtmlPart):
-    def __init__(self, elt = None, p_class = None, p_name = None, p_id = None):
-        self.scripts_add("jkd.js")
+    def __init__(self, elt = None, p_class = None, p_name = None, p_id = None, **kwargs):
+        super().__init__(elt, p_class, p_name, p_id, **kwargs)
+        parms = {'p_id':self.p_id}
+        self.css_add("jkd.css")
+        self.script_add("jkd.js")
+        self.script_add("jkd.js")
+        self.script_add("jkd.js")
+        self.script_add("jkd.js")
+        self.script_add("jkd.js")
+        self.html_template = jinja2.Template("""
+  <!-- HtmlPartHisto {{p_id}} -->
+  <div id="{{p_id}}-container">
+    <canvas id="{{p_id}}-canvas" width="800" height="100"/>
+  </div>
+  <!-- HtmlPartHisto end -->""")
+        self.js_template = jinja2.Template("""
+  // HtmlPartHisto {{p_id}} script part
+  var {{p_id}}_ctx = $("#{{p_id}}-canvas");
+
+  var {{p_id}}_chart = new Chart({{p_id}}_ctx, {
+    type: 'line',
+    data: {
+//        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            xAxes:[{
+//              autoSkip:true,
+//              autoSkipPadding:10,
+                minRotation:45,
+                type: 'time',
+                time:{
+//                  unit:'millisecond',
+                    displayFormats:{
+                    millisecond:'HH:mm:ss.S',
+                    second:'HH:mm:ss',
+                    minut:'HH:mm',
+                }
+            }
+        }],
+            yAxes: [{
+                ticks: {
+                    //beginAtZero:true
+                }
+            }]
+        },
+        zoom:{
+            enabled:true,
+            mode:'xy',
+        },
+        pan:{
+            enabled:true,
+            mode:'xy',
+        },
+    },
+});
+  // end of HtmlPartHisto script part
+""")
 
 
 class HtmlPage(Node):
     tagname = "html_page"
     def __init__(self, elt = None, parent = None, **kwargs):
+        self.parts_registry = {'histo':HtmlPartHisto,
+                               'value':HtmlPartValue,
+                               'table':HtmlPartTable,
+                               'chart':HtmlPartChart}
         if 'appname' in kwargs:
             self.appname = kwargs['appname']
         else:
@@ -59,28 +157,33 @@ class HtmlPage(Node):
         defs = [] # already defined id's
         for part_node in elt:
             self.debug("  Page part: {}".format(part_node.tag))
-            part = dict(part_node.attrib)
-            part['jkd_class'] = str(part_node.tag)
-            if "id" not in part:
+            part = self.parts_registry[part_node.tag](**part_node.attrib)
+            #part['jkd_class'] = str(part_node.tag)
+            if part.p_id is None:
                 n = 1
                 while str(part_node.tag) + '_' + str(n) in defs:
                     n += 1
-                part["id"] = str(part_node.tag) + '_' + str(n)
-            defs.append(part["id"])
-            if "class" not in part:
-                part["class"] = str(part_node.tag)
+                part.p_id = str(part_node.tag) + '_' + str(n)
+            defs.append(part.p_id)
+            if part.p_class is None:
+                part.p_class = str(part_node.tag)
             self.parts.append(part)
             #self.parts.append({'template':self.jinja_env.get_template(part.attrib['template'] + '.jinja2')})
 
     async def generate(self, args={}):
-        scripts = []
-        css = []
-        html = []
-        js = []
+        scripts = ['jquery-3.3.1.min.js']
+        css_list = []
+        html = ""
+        js = ""
         for part in self.parts:
-            pass
+            for script in part.scripts:
+                if script not in scripts:
+                    scripts.append(script)
+            for css in part.css:
+                if css not in css_list:
+                    css_list.append(css)
+            html += part.get_html()
+            js += part.get_js()
         template = self.jinja_env.get_template(self.name + '.jinja2')
-        html_page = template.render({'pagetitle':self.pagetitle, 'name':'Joris', 'parts':self.parts})
+        html_page = template.render({'pagetitle':self.pagetitle, 'name':'Joris', 'css_list':css_list, 'scripts':scripts, 'js':js, 'html':html})
         return html_page
-
-
