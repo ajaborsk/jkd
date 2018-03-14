02cb45e5c41ac0020b1b81b143d0a613657e8cc9
ajaborsk <alexandre.jaborska@laposte.net>
2018-03-14 07:58:15 +0000

    Some renaming and cleanup

--- jkd/html_page.py
+++ jkd/html_page.py
@@ -78,80 +78,8 @@ class HtmlPartHisto(HtmlPart):
         self.js_template = jinja2.Template("""
   // HtmlPartHisto {{p_id}} script part
 
-  var hc = new JkdHistoryChart(env, "{{p_id}}", "{{data_addr}}");
-""")
-        dummy = ("""
-  var {{p_id}}_ctx = $("#{{p_id}}-canvas");
+  var hc = new JkdHistoryChart(jkd_env, "{{p_id}}", "{{data_addr}}");
 
-  var {{p_id}}_chart = new Chart({{p_id}}_ctx, {
-    type: 'line',
-    data: {
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            xAxes:[{
-                minRotation:45,
-                type: 'time',
-                time:{
-                    displayFormats:{
-                    millisecond:'HH:mm:ss.S',
-                    second:'HH:mm:ss',
-                    minut:'HH:mm',
-                }
-            }
-        }],
-            yAxes: [{
-                ticks: {
-                    //beginAtZero:true
-                }
-            }]
-        },
-        zoom:{
-            enabled:true,
-            mode:'y',
-        },
-        pan:{
-            enabled:true,
-            mode:'y',
-        },
-    },
-});
-
-  $("#{{p_id}}-update").click(function (evt) {
-    env.get('{{data_addr}}', {'after':Date.now() / 1000 - 3600 * 3},
-      function (msg, client)
-       {
-        {{p_id}}_chart.data = msg.reply;
-        {{p_id}}_chart.update();
-       },
-      null);
-    }
-  );
-
-  $("#{{p_id}}-zreset").click(function (evt) {
-    {{p_id}}_chart.resetZoom();
-  });
   // end of HtmlPartHisto script part
 """)
 
