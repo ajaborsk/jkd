31b356458caf3150813b693b1a82132d08e94f18
ajaborsk <alexandre.jaborska@laposte.net>
2018-03-15 17:33:21 +0100

    Internal battery voltage

--- jkd/data_process_0.py
+++ jkd/data_process_0.py
@@ -25,6 +25,7 @@ class DataProcess0(DataProcess):
         labels = []
         datasets = [{'label':'V Bat', 'yAxisID':'voltage', 'borderWidth':1, 'borderColor':'blue', 'fill':False, 'pointRadius':0, 'lineTension':0, 'data':[]},
                     {'label':'V Cir', 'yAxisID':'voltage', 'borderWidth':1, 'borderColor':'red', 'fill':False, 'pointRadius':0, 'lineTension':0, 'data':[]},
+                    {'label':'V Int', 'yAxisID':'voltage', 'borderWidth':1, 'borderColor':'black', 'fill':False, 'pointRadius':0, 'lineTension':0, 'data':[]},
                     {'label':'T Ext ', 'yAxisID':'temp', 'borderWidth':1, 'borderColor':'magenta', 'fill':False, 'pointRadius':0, 'lineTension':0, 'data':[]},
                     {'label':'T Mcu', 'yAxisID':'temp', 'borderWidth':1, 'borderColor':'cyan', 'fill':False, 'pointRadius':0, 'lineTension':0, 'data':[]},
                     {'label':'I Bat', 'yAxisID':'intensity', 'borderWidth':1, 'borderColor':'orange', 'fill':False, 'pointRadius':0, 'lineTension':0, 'data':[]},
@@ -35,11 +36,12 @@ class DataProcess0(DataProcess):
         #datasets[1]['data'] = list(data[2])
         datasets[0]['data'] = list(data['v_bat'])
         datasets[1]['data'] = list(data['v_cir'])
+        datasets[2]['data'] = list(data['v_int'])
         #datasets[2]['data'] = list(data[3])
         #datasets[3]['data'] = list(data[4])
-        datasets[2]['data'] = list(data['t_ext'])
-        datasets[3]['data'] = list(data['t_mcu'])
-        datasets[4]['data'] = list(data['i_bat'])
+        datasets[3]['data'] = list(data['t_ext'])
+        datasets[4]['data'] = list(data['t_mcu'])
+        datasets[5]['data'] = list(data['i_bat'])
 
         response = {'labels':labels, 'datasets':datasets}
         return response
