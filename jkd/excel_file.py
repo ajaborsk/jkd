678e7ad4a8002b72b5399c6ab97052cda130e940
Alexandre JABORSKA <alexandre.jaborska@laposte.net>
2018-03-30 13:50:34 +0200

    Preliminary excel file implementation (only reading, no indexes)

--- jkd/excel_file.py
+++ jkd/excel_file.py
@@ -1,3 +1,5 @@
+import itertools
+
 import pandas as pd
 import openpyxl
 
@@ -8,20 +10,39 @@ class ExcelFile(DataFile):
     def __init__(self, file="", elt = None, **kwargs):
         super().__init__(elt=elt, **kwargs)
 
-        self.data = None
-        self.filename = file
+        #self.data = None
+        fqn = self.fqn().split('/')[1:]
+        self.filename = fqn[0] + '/var/' + file
 
         #self.port_add('output', cached = False, timestamped = True)
 
         self.sheets = []
+        output_port_names = []
         for el in elt:
             if el.tag == "sheet":
                 self.sheets.append(dict(el.attrib))
-                self.port_add(el.get("port", "sheet"))
+                self.port_add(el.get("port", "sheet"), timestamped=False)
+                output_port_names.append(el.get("port", "sheet"))
 
-        self.task_add('process', coro = self.process, gets=[], returns=list(self.ports.keys()))
+        self.task_add('process', coro = self.process, gets=[], returns=list(output_port_names))
 
     async def process(self, args={}):
-        data = pd.DataFrame([[j+i*0.01 for j in range(10)] for i in range(10)])
-        return (0, data.iloc[:100])
+        wb = openpyxl.load_workbook(self.filename)
+        data = []
+        for sheet in self.sheets:
+            data_sheet = wb[sheet['name']]
+            sheet_data = data_sheet.values
+            cols = next(sheet_data)[:]
+            sheet_data = list(sheet_data)
+            #idx = ["{:04d}-{:05d}".format(r[0], r[1]) for r in sheet_data]
+            #uidx = ["{:04d}".format(r[2]) for r in sheet_data]
+            sheet_data = (itertools.islice(r, 0, None) for r in sheet_data)
+            #df = pd.DataFrame(sheet_data, index=pd.MultiIndex.from_arrays([idx, uidx]), columns=cols)
+            df = pd.DataFrame(sheet_data, columns=cols)
+            data.append(df)
+        if len(data) == 1:
+            data = data[0]
+        #data = pd.DataFrame([[j+i*0.01 for j in range(10)] for i in range(10)])
+        return data
+        #return (0, {'ports':self.ports, 'sheets': self.sheets, 'data':data.iloc[:100]})
 
