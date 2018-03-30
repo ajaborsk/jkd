a7ca78f86669d40f31dd0dadfc98d4811f9921a7
ajaborsk <alexandre.jaborska@laposte.net>
2018-03-30 07:39:48 +0200

    Some preliminary work on xlsx reading

--- jkd/excel_file.py
+++ jkd/excel_file.py
@@ -1,22 +1,27 @@
-import asyncio
-import time
-import datetime
-import math
-
+import pandas as pd
 import openpyxl
 
 from .data_file import DataFile
 
 class ExcelFile(DataFile):
     tagname = "excel_file"
-    def __init__(self, elt = None, **kwargs):
+    def __init__(self, file="", elt = None, **kwargs):
         super().__init__(elt=elt, **kwargs)
 
         self.data = None
+        self.filename = file
 
-        self.port_add('output', cached = False, timestamped = True)
-        self.task_add('process', coro = self.parse, gets=['input'], returns=['output'])
+        #self.port_add('output', cached = False, timestamped = True)
 
-    async def process(self, line):
-        return self.data
+        self.sheets = []
+        for el in elt:
+            if el.tag == "sheet":
+                self.sheets.append(dict(el.attrib))
+                self.port_add(el.get("port", "sheet"))
+
+        self.task_add('process', coro = self.process, gets=[], returns=list(self.ports.keys()))
+
+    async def process(self, args={}):
+        data = pd.DataFrame([[j+i*0.01 for j in range(10)] for i in range(10)])
+        return (0, data.iloc[:100])
 
