5818937ca3edfe50a569946ba64314eae5e55ba6
Alexandre JABORSKA <alexandre.jaborska@laposte.net>
2018-04-24 12:49:08 +0200

    Syntax error

--- jkd/node.py
+++ jkd/node.py
@@ -129,7 +129,7 @@ class Node:
 
     def port_get(self, portname, args={}):
         # get a port and create a dynamic port from a factory if needed (and possible)
-        if portname in self.ports
+        if portname in self.ports:
             if self.ports[portname].get('factory', False):
                 d_portname = portname+'-'+hex(hash(frozenset(args.items())) % ((sys.maxsize+1)*2))[2:]
                 #TODO
